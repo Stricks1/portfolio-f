bd7a58200763fecd1de9a22236fc868f3093b1c3
Stricks1 <gmalheiross@gmail.com>
2020-12-16 18:19:33 -0300

    layout header for phone

--- src/containers/headerContainer.js
+++ src/containers/headerContainer.js
@@ -9,8 +9,38 @@ const Header = () => {
 
   if (width < breakpoint) {
     return (
-      <div className="padding-top-base vh-100 header-container">
-        MOBILE VERSION
+      <div className="padding-top-base h-600 header-container d-flex justify-content-center bg-header-img-mobile" style={{ backgroundImage: `url(${HeaderImage})` }}>
+        <div className="d-flex flex-column mx-4 align-self-center">
+          <div className="align-self-center justify-self-end max-info-header">
+            <h1>Hey There. <br /> I'm Gabriel</h1>
+            <h4>I'm a Software Developer</h4>
+            <p>I can help you build a product, feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don't hesitate to contact me.</p>
+          </div>
+          <div className="align-self-center w-50">
+            <ul className="list-unstyled list-group list-header d-flex flex-row justify-content-between w-100">
+              <li>
+                <a href="https://github.com/Stricks1/" target="_blank" title="GitHub" className="grey-color">
+                  <i class="fa fa-github" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/gabriel-malheiros-silveira/" target="_blank" title="linkedin" className="grey-color">
+                  <i class="fa fa-linkedin" />
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/gabriel_stricks" target="_blank" title="twitter" className="grey-color">
+                  <i class="fa fa-twitter" />
+                </a>
+              </li>
+              <li>
+                <a href="https://angel.co/u/gabriel-malheiros-silveira" target="_blank" title="angellist" className="grey-color">
+                  <i class="fa fa-angellist" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
@@ -42,12 +72,12 @@ const Header = () => {
             </li>
           </ul>
         </div>
-        <div className="align-self-center justify-self-end max-info-header">
+        <div className="align-self-center justify-self-end max-info-header header-left-desktop">
           <h1>Hey There. <br /> I'm Gabriel</h1>
           <h4>I'm a Software Developer</h4>
           <p>I can help you build a product, feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don't hesitate to contact me.</p>
         </div>
-        <div className="header-image2">
+        <div>
           <div className="bg-header-img" style={{ backgroundImage: `url(${HeaderImage})` }} />
         </div>
       </div>
