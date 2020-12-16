48f74e60f881cec569145c7f6e6d905ffc75ab61
Stricks1 <gmalheiross@gmail.com>
2020-12-16 14:46:55 -0300

    navbar created for phone and desktop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ViewportProvider } from "./hooks/viewPort";
+import Nav from "./components/topNav";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ViewportProvider>
+      <Nav />
+      <div className="width-big"></div>
+      <div id="Portfolio">
+      TEST
+      </div>
+      <div className="width-big"></div>
+    </ViewportProvider>
   );
 }
-
-export default App;
