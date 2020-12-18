432ff8e5416d919ea366b7fc1b3b65643f701ede
Stricks1 <gmalheiross@gmail.com>
2020-12-18 00:08:32 -0300

    initial project creating

---
+++ src/components/project.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { useViewport } from "../hooks/viewPort";
+
+const Project = () => {
+  const { width } = useViewport();
+  const breakpoint = 560;
+  const toggleMobileMenu = () => {
+    const navStart = document.getElementById('nav-links');
+    const menuM = document.getElementById('dropdown-menu');
+    menuM.lastChild.classList.toggle('d-none');
+    navStart.firstChild.classList.toggle('invisible');
+    menuM.firstChild.firstChild.classList.toggle('fa-bars');
+    menuM.firstChild.firstChild.classList.toggle('fa-times');
+    menuM.firstChild.firstChild.classList.toggle('nav-clicked');
+    console.log(menuM.firstChild.firstChild);
+  };
+
+  if (width < breakpoint) {
+    return (
+      <div id="nav-links" className="px-3 py-2 w-100 d-flex justify-content-between text-white blurrable">
+        <div className="mr-4 align-self-center nav-name">
+          Gabriel
+        </div>
+        <div id="dropdown-menu">
+          <div onClick={toggleMobileMenu}>
+            <i className="fas fa-bars" />
+          </div>
+
+          <nav className="d-none nav-expanded">
+            <ul className="list-unstyled list-group text-white">
+              <li className="pl-2 m-auto item-drop">
+                <a href="#Portfolio" onClick={toggleMobileMenu} className="text-white">
+                  Portfolio
+                </a>
+              </li>
+              <li className="pl-2 m-auto item-drop">
+                <a href="#About" onClick={toggleMobileMenu} className="text-white">
+                  About
+                </a>
+              </li>
+              <li className="pl-2 m-auto item-drop">
+                <a href="#Contact" onClick={toggleMobileMenu} className="text-white">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <nav id="nav-links" className="py-2 w-100 d-flex justify-content-between text-white blurrable">
+      <div className="ml-3 align-self-center nav-name">
+        Gabriel
+      </div>
+      <ul className="d-flex list-nav justify-content-between list-fullscreen">
+        <li className="pl-2">
+          <a href="#Portfolio" className="text-white text-decoration-none">
+            Portfolio
+          </a>
+        </li>
+        <li className="pl-2">
+          <a href="#About" className="text-white text-decoration-none">
+            About
+          </a>
+        </li>
+        <li className="pl-2">
+          <a href="#Contact" className="text-white text-decoration-none">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Project;
--- src/containers/projectsContainer.js
+++ src/containers/projectsContainer.js
@@ -3,7 +3,10 @@ import { useViewport } from "../hooks/viewPort";
 import frontHouses from "../assets/frontendhouses.png";
 /*import phoneMusic from "../assets/onlineShopCel.png";
 import desktopMusic from "../assets/onlineShop.png";*/
+import seaSs from "../assets/seaSS.png";
+import seaBg from "../assets/seaBg.png";
 import HighlightProject from "../components/mainProject";
+import Project from "../components/project";
 
 const Projects = () => {
   const { width } = useViewport();
@@ -14,10 +17,22 @@ const Projects = () => {
     info: "On this project I created a Front End that uses my API to manage renting places. The idea is to create a simplified version of AirBnb, where every user can register their own places for rent. It handle the user signing (utilizing tokens for validating the sessions), each user can create edit and delete their own places adding photos. And manage their favorites places.",
     techs: ["React", "Redux", "Ruby on Rails", "API"],
     images: [frontHouses], 
-  /* images: [desktopMusic, phoneMusic], */
+    background: [],
     github: "https://github.com/Stricks1/houses-front-end",
     live: "https://gallant-ptolemy-3e1fa6.netlify.app/",
   }
+
+  const listProjects = [
+    {
+      name: "Sea Treasures",
+      info: "On this game, you are Bob the octopus, and you are in a quest to find a treasure in the 16-bit layout deep sea. You need to face some sea creatures in order to achieve your goal, and you can kill the creatures by shooting ink from our hero and interact with levers on the scenario to open paths. You have a limited time to complete the quest.",
+      techs: ["JS", "Phaser3", "Game"],
+      images: [seaSs],
+      background: [seaBg],
+      github: "https://github.com/Stricks1/shooterGame",
+      live: "https://phaserseagame.herokuapp.com/",
+    }
+  ]
 
   if (width < breakpoint) {
     return (
@@ -27,6 +42,11 @@ const Projects = () => {
           <span className="line-separator mb-4"></span>
         </div>
         <HighlightProject project={highlightProject}/>
+        <div className="cards-container">
+          {
+            listProjects.map(project => (<Project key={project.name} project={project}  />))
+          }
+        </div>
       </div>
     );
   }
@@ -38,6 +58,11 @@ const Projects = () => {
         <span className="line-separator ml-3"></span>
       </div>
       <HighlightProject project={highlightProject}/>
+      <div className="cards-container">
+        {
+          listProjects.map(project => (<Project key={project.name} project={project} />))
+        }
+      </div>
     </div>
   );
 };
