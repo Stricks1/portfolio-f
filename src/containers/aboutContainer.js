e7c1d727683ddac76f08a1889fc9676cff8a702f
Stricks1 <gmalheiross@gmail.com>
2020-12-18 21:58:39 -0300

    add resume button on about section

--- src/containers/aboutContainer.js
+++ src/containers/aboutContainer.js
@@ -9,9 +9,30 @@ const About = () => {
 
   if (width < breakpoint) {
     return (
-      <div id="About" className="padding-top-base h-600 header-container d-flex justify-content-center bg-about-img-mobile blurrable" style={{ backgroundImage: `url(${AboutImage})` }}>
-        <div className="d-flex flex-column mx-4 align-self-center pb-4">
-          <div className="align-self-center justify-self-end max-info-header">
+      <div className="d-flex flex-column padding-top-base h-600 header-container d-flex justify-content-center ">
+        <div id="About" className="padding-top-base h-600 header-container d-flex justify-content-center bg-about-img-mobile blurrable" style={{ backgroundImage: `url(${AboutImage})` }}>  
+          <div className="d-flex flex-column mx-4 align-self-center pb-4">
+            <div className="align-self-center justify-self-end max-info-header">
+              <h1>About me</h1>
+              <p>Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.</p>
+              <div className="d-flex">
+                <div className="button-square py-1 px-3 mr-2 mb-2 font-weight-bold" onClick={() => window.open('https://docs.google.com/document/d/18XKdsjUNB2lycjNyZZL-U_fCFK1goj0bV22lHz1pSWs/export?format=pdf', '_blank')}>
+                  Get My Resume
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <span class="line-separator-white mb-4"></span>
+      </div>
+    );
+  }
+
+  return (
+    <div id="About" className="padding-top-base header-container blurrable">
+      <div className="d-flex flex-column">
+        <div className="d-flex justify-content-between pb-4">
+          <div className="align-self-center justify-self-end max-info-header about-left-desktop">
             <h1>About me</h1>
             <p>Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.</p>
             <div className="d-flex">
@@ -20,26 +41,11 @@ const About = () => {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div id="About" className="padding-top-base header-container blurrable">
-      <div className="d-flex justify-content-between pb-4">
-        <div className="align-self-center justify-self-end max-info-header header-left-desktop">
-          <h1>About me</h1>
-          <p>Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.</p>
-          <div className="d-flex">
-            <div className="button-square py-1 px-3 mr-2 mb-2 font-weight-bold" onClick={() => window.open('https://docs.google.com/document/d/18XKdsjUNB2lycjNyZZL-U_fCFK1goj0bV22lHz1pSWs/export?format=pdf', '_blank')}>
-              Get My Resume
-            </div>
+          <div>
+            <div className="bg-about-img" style={{ backgroundImage: `url(${AboutImage})` }} />
           </div>
         </div>
-        <div>
-          <div className="bg-about-img" style={{ backgroundImage: `url(${AboutImage})` }} />
-        </div>
+        <span class="line-separator-white mb-4 vw-90"></span>
       </div>
     </div>
   );
