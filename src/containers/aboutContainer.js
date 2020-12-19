a8f56eac4bd7e3b975c6e66e1d661b7d91c09ce9
Stricks1 <gmalheiross@gmail.com>
2020-12-19 16:11:04 -0300

    added list about me

--- src/containers/aboutContainer.js
+++ src/containers/aboutContainer.js
@@ -2,10 +2,33 @@
 import React from 'react';
 import { useViewport } from "../hooks/viewPort";
 import AboutImage from "../assets/about.svg";
+import Square45 from "../assets/square45.svg";
+import Square from "../assets/square.svg";
+import Circle from "../assets/circle.svg";
 
 const About = () => {
   const { width } = useViewport();
   const breakpoint = 560;
+  const Languages = [
+    'Javascript',
+    'Ruby',
+    'Html',
+    'Css',
+    'Cobol'
+  ];
+  const Frameworks = [
+    'Bootstrap',
+    'Ruby on Rails',
+    'RSpec',
+    'CapyBara',
+    'React&Redux'    
+  ];
+  const Skills = [
+    'Github',
+    'Terminal',
+    'Communication',
+    'Remote Working'
+  ];
 
   if (width < breakpoint) {
     return (
@@ -14,7 +37,7 @@ const About = () => {
           <div className="d-flex flex-column mx-4 align-self-center pb-4">
             <div className="align-self-center justify-self-end max-info-header">
               <h1>About me</h1>
-              <p>Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.</p>
+              <p>Hi, I'm Gabriel, an enthusiastic problem solver. I'm from Brazil. I have a Bachelors in Information Systems and I studied Software Engineering at Microverse. I have more than 5 years experience in mainframe and web programming. I also have experience with other tools/languages listed below. If you'd like to get in contact about potential job opportunities, please reach out!</p>
               <div className="d-flex">
                 <div className="button-square py-1 px-3 mr-2 mb-2 font-weight-bold" onClick={() => window.open('https://docs.google.com/document/d/18XKdsjUNB2lycjNyZZL-U_fCFK1goj0bV22lHz1pSWs/export?format=pdf', '_blank')}>
                   Get My Resume
@@ -23,7 +46,7 @@ const About = () => {
             </div>
           </div>
         </div>
-        <span class="line-separator-white mb-4"></span>
+        <span className="line-separator-white mb-4"></span>
       </div>
     );
   }
@@ -34,7 +57,7 @@ const About = () => {
         <div className="d-flex justify-content-between pb-4">
           <div className="align-self-center justify-self-end max-info-header about-left-desktop">
             <h1>About me</h1>
-            <p>Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.</p>
+            <p>Hi, I'm Gabriel, an enthusiastic problem solver. I'm from Brazil. I have a Bachelors in Information Systems and I studied Software Engineering at Microverse. I have more than 5 years experience in mainframe and web programming. I also have experience with other tools/languages listed below. If you'd like to get in contact about potential job opportunities, please reach out!</p>
             <div className="d-flex">
               <div className="button-square py-1 px-3 mr-2 mb-2 font-weight-bold" onClick={() => window.open('https://docs.google.com/document/d/18XKdsjUNB2lycjNyZZL-U_fCFK1goj0bV22lHz1pSWs/export?format=pdf', '_blank')}>
                 Get My Resume
@@ -45,7 +68,51 @@ const About = () => {
             <div className="bg-about-img" style={{ backgroundImage: `url(${AboutImage})` }} />
           </div>
         </div>
-        <span class="line-separator-white mb-4 vw-90"></span>
+        <span className="line-separator-white mb-4 vw-90"></span>
+      </div>
+      <div className="d-flex justify-content-around">
+        <div className="d-flex flex-column">
+          <div className="d-flex align-self-start flex-nowrap mb-3 justify-content-center align-items-center">
+            <img className="bulletLists45" src={Square45} alt="square 45 bullet"/>
+            <h5 className="pt-2 font-weight-bold">&nbsp;Language</h5>
+          </div>
+          <ul className="ul-about list-unstyled list-group font-list-about d-flex flex-column flex-nowrap">
+            { Languages.map(item => (
+                <li key={item}>
+                    {item}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="d-flex flex-column">
+          <div className="d-flex align-self-start flex-nowrap mb-3 justify-content-center align-items-center">
+            <img className="bulletLists" src={Square} alt="square 45 bullet"/>
+            <h5 className="pt-2 font-weight-bold">&nbsp;Frameworks</h5>
+          </div>
+          <ul className="ul-about list-unstyled list-group font-list-about d-flex flex-column flex-nowrap">
+            { Frameworks.map(item => (
+                <li key={item}>
+                    {item}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="d-flex flex-column">
+          <div className="d-flex align-self-start flex-nowrap mb-3 justify-content-center align-items-center">
+              <img className="bulletLists" src={Circle} alt="square 45 bullet"/>
+              <h5 className="pt-2 font-weight-bold">&nbsp;Skills</h5>
+          </div>
+          <ul className="ul-about list-unstyled list-group font-list-about d-flex flex-column flex-nowrap">
+            { Skills.map(item => (
+                <li key={item}>
+                    {item}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
       </div>
     </div>
   );
