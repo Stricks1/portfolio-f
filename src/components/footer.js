88fe1b83ae227ce346dd42654e882bfeac3b3a32
Stricks1 <gmalheiross@gmail.com>
2020-12-20 00:31:02 -0300

    change color links on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,22 +12,22 @@ const Footer = () => {
       <div className="mt-2 border-footer d-flex flex-column justify-content-center align-items-center text-white blurrable">
         <ul className="my-2 list-unstyled footer-width list-group list-header d-flex flex-row justify-content-between">
              <li>
-            <a href="https://github.com/Stricks1/" target="_blank" title="GitHub" className="grey-color">
+            <a href="https://github.com/Stricks1/" target="_blank" title="GitHub" className="grey-color2">
               <i className="fa fa-github" />
             </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/gabriel-malheiros-silveira/" target="_blank" title="linkedin" className="grey-color">
+            <a href="https://www.linkedin.com/in/gabriel-malheiros-silveira/" target="_blank" title="linkedin" className="grey-color2">
               <i className="fa fa-linkedin" />
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/gabriel_stricks" target="_blank" title="twitter" className="grey-color">
+            <a href="https://twitter.com/gabriel_stricks" target="_blank" title="twitter" className="grey-color2">
               <i className="fa fa-twitter" />
             </a>
           </li>
           <li>
-            <a href="https://angel.co/u/gabriel-malheiros-silveira" target="_blank" title="angellist" className="grey-color">
+            <a href="https://angel.co/u/gabriel-malheiros-silveira" target="_blank" title="angellist" className="grey-color2">
               <i className="fa fa-angellist" />
             </a>
           </li>
@@ -43,22 +43,22 @@ const Footer = () => {
     <div className="mt-2 border-footer d-flex justify-content-center text-white blurrable">
       <ul className="my-2 list-unstyled footer-width list-group list-header d-flex flex-row justify-content-between">
         <li>
-          <a href="https://github.com/Stricks1/" target="_blank" title="GitHub" className="grey-color">
+          <a href="https://github.com/Stricks1/" target="_blank" title="GitHub" className="grey-color2">
             <i className="fa fa-github" />
           </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/gabriel-malheiros-silveira/" target="_blank" title="linkedin" className="grey-color">
+          <a href="https://www.linkedin.com/in/gabriel-malheiros-silveira/" target="_blank" title="linkedin" className="grey-color2">
             <i className="fa fa-linkedin" />
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/gabriel_stricks" target="_blank" title="twitter" className="grey-color">
+          <a href="https://twitter.com/gabriel_stricks" target="_blank" title="twitter" className="grey-color2">
             <i className="fa fa-twitter" />
           </a>
         </li>
         <li>
-          <a href="https://angel.co/u/gabriel-malheiros-silveira" target="_blank" title="angellist" className="grey-color">
+          <a href="https://angel.co/u/gabriel-malheiros-silveira" target="_blank" title="angellist" className="grey-color2">
             <i className="fa fa-angellist" />
           </a>
         </li>
